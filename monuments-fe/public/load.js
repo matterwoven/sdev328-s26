231d50e587e79e484a893eb2e9c9172b6c306ae0
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2026-05-21 13:09:49 -0700

    Porposie

--- monuments-fe/public/load.js
+++ monuments-fe/public/load.js
@@ -25,7 +25,7 @@ async function handleSubmit(event) {
     }
     console.log(formValues);
 
-    const url = "http://localhost:8000/api/monuments";
+    const url = "http://localhost:8001/api/monuments";
     const config = {
         method: "post",
         headers: {
@@ -38,9 +38,43 @@ async function handleSubmit(event) {
     console.log("Form submitted!");
 }
 
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    //get our form values
+    const name = document.querySelector("#name").value;
+    const yearCompleted = document.querySelector("#yearCompleted").value;
+    const type = document.querySelector("#type").value;
+    const heightFeet = document.querySelector("#heightFeet").value;
+    const theme = document.querySelector("#theme").value;
+
+    const formValues = {
+        name,
+        yearCompleted,
+        type,
+        heightFeet,
+        theme
+    }
+    console.log(formValues);
+
+    const url = "http://localhost:8001/api/monuments";
+    const config = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formValues)
+    }
+    
+
+
+    const response = await fetch(url, config);
+    console.log(response);
+    console.log("Form submitted!");
+}
 //connect to API + gather data
 async function loadMonuments() {
-    const url = "http://localhost:8000/api/monuments";
+    const url = "http://localhost:8001/api/monuments";
     const config = {
         method: "get"
     }
@@ -64,17 +98,110 @@ function showMonuments(mons) {
     const grid = document.querySelector("#monuments-grid");
 
     for (let monument of mons) {
-        const { name, yearCompleted, type } = monument;
+        addMonumentCard(monument, grid);
+        // const { name, yearCompleted, type } = monument;
 
-        const monumentCard = `
-            <div class="monument">
-                <h2>${name}</h2>
-                <hr>
-                <p class="year">Year: ${yearCompleted}</p>
-                <p>Type: ${type}</p>
-            </div>
-        `
+        // const monumentCard = `
+        //     <div class="monument">
+        //         <h2>${name}</h2>
+        //         <hr>
+        //         <p class="year">Year: ${yearCompleted}</p>
+        //         <p>Type: ${type}</p>
+        //     </div>
+        // `
 
-        grid.innerHTML += monumentCard;
+        // grid.innerHTML += monumentCard;
     }
+}
+
+function editMonumentHandler(monumnt, grid){
+    //create elements
+    const { name, yearCompleted, type } = monument;
+    const [div, h2, hr, pYear, pType, ulLinks, liEdit, liDelete, aEdit, aDelete] = 
+    createElements(["div", "h2", "hr", "p", "p", "ul", "li", "li", "a", "a"]);
+
+    //configure them
+    div.className = "monument";
+    h2.textContent = name;
+    pYear.className = "year";
+    pYear.textContent = `Year: ${yearCompleted}`;
+    pType.textContent = `Type: ${type}`;
+
+    aEdit.textContent = "Edit";
+    aEdit.href = "#";
+    aEdit.textContent = "Delete"
+    aEdit.href = "#";
+    //connect our list of links
+    liEdit.appendChild(aEdit);
+    liDelete.appendChild(aDelete);
+    appendAll(ulLinks, [liEdit, liDelete]);
+
+    liEdit.onclick = () => {
+        event.preventDefault();
+        editMonument(monument);
+    }
+    //add them to the dom
+    appendAll(div, [h2, hr, pYear, pType]);
+    grid.appendChild(div);
+
+}
+
+function addMonumentCard(monumnt, grid){
+    //create elements
+    const { name, yearCompleted, type } = monument;
+    const [div, h2, hr, pYear, pType, ulLinks, liEdit, liDelete, aEdit, aDelete] = 
+    createElements(["div", "h2", "hr", "p", "p", "ul", "li", "li", "a", "a"]);
+
+    //configure them
+    div.className = "monument";
+    h2.textContent = name;
+    pYear.className = "year";
+    pYear.textContent = `Year: ${yearCompleted}`;
+    pType.textContent = `Type: ${type}`;
+
+    aEdit.textContent = "Edit";
+    aEdit.href = "#";
+    aEdit.textContent = "Delete"
+    aEdit.href = "#";
+    //connect our list of links
+    liEdit.appendChild(aEdit);
+    liDelete.appendChild(aDelete);
+    appendAll(ulLinks, [liEdit, liDelete]);
+
+    liEdit.onclick = () => {
+        event.preventDefault();
+        editMonument(monument);
+    }
+    //add them to the dom
+    appendAll(div, [h2, hr, pYear, pType]);
+    grid.appendChild(div);
+
+}
+
+function editMonument(monument) {
+    console.log(monument);
+    
+    document.querySelector("#name").value = name;
+    document.querySelector("#yearCompleted").value = yearCompleted;
+    document.querySelector("#type").value = type;
+    document.querySelector("#heightFeet").value = heightFeet;
+    document.querySelector("#theme").value = theme;
+}
+
+function deleteMonument(monument) {
+
+}
+
+function appendAll(parent, children){
+    for (const child of children){
+        parent.appendChild(child);
+    }
+}
+
+function createElements(tags){
+    const elems = [];
+    for (const tag of tags){
+        elems.push(document.createElement(tag));
+    }
+    return elems;
 }
--- monuments-fe/src/server.js
+++ monuments-fe/src/server.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static("public"));
 
 const port = 8001;
-app.listen(port, () => console.log(`Front end started on http://localhost:${port}`));
+app.listen(port, () => console.log(`Front end started on http://localhost:${port}/monuments.html`));
